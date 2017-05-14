46d9ce2cbe8d94bab091e23e59b305ada7aee039
Simon Laing <laingster@gmail.com>
2017-05-14 21:30:58 +0100

    Updates to support flexible parameter values, looking for value by name and supporting non-anonymous objects

--- Dapper.MoqTests/MatchAnonymousObjectExpressionVisitor.cs
+++ Dapper.MoqTests/MatchAnonymousObjectExpressionVisitor.cs
@@ -16,21 +16,39 @@
             return methodCallExpression.Method;
         }
 
-        protected override Expression VisitNew(NewExpression node)
+        protected override Expression VisitMethodCall(MethodCallExpression node)
         {
-            var anonymousType = node.Type.Name.Contains("AnonymousType");
-            if (!anonymousType)
-                return node;
+            var arguments = node.Arguments.Select(ReduceToConstant).ToArray();
+            var argumentLookup = arguments
+                .Zip(node.Method.GetParameters(), (value, param) => new { name = param.Name, value });
 
-            var newArguments = node.Arguments.Select(ExpressionHelper.GetValue).ToArray();
-            var anonymousObject = node.Constructor.Invoke(newArguments);
-            var predicateExpression = Expression.Constant(new Predicate<object>(actual => PropertiesExistsAndMatch(actual, anonymousObject)));
+            var newArguments = from argument in argumentLookup
+                               select argument.name.Equals("parameters", StringComparison.OrdinalIgnoreCase)
+                                    ? GetNewArgument(argument.value)
+                                    : Expression.Constant(argument.value);
 
+            return Expression.Call(node.Object, node.Method, newArguments);
+        }
+
+        private static object ReduceToConstant(Expression expression)
+        {
+            var objectMember = Expression.Convert(expression, typeof(object));
+            var getterLambda = Expression.Lambda<Func<object>>(objectMember);
+            var getter = getterLambda.Compile();
+            return getter();
+        }
+
+        private Expression GetNewArgument(object value)
+        {
+            var predicateExpression = Expression.Constant(new Predicate<object>(actual => PropertiesExistsAndMatch(actual, value)));
             return Expression.Call(null, createMatch, predicateExpression);
         }
 
         private static bool PropertiesExistsAndMatch(object actual, object other)
         {
+            if (ReferenceEquals(other, MockDbConnection.Any) || ReferenceEquals(other, SqlText.Any))
+                return true;
+
             var actualParams = actual as MockDbParameterCollection ?? new MockDbParameterCollection(actual);
             var otherParams = new MockDbParameterCollection(other);
 
