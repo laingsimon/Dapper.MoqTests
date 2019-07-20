ecb2fbcd242ef13abbd2fcfb6cc946d73f829dae
Simon Laing <laingster@gmail.com>
2019-07-20 08:24:44 +0100

    Include Dapper method descriptions

--- Dapper.MoqTests/IMockDatabase.cs
+++ Dapper.MoqTests/IMockDatabase.cs
@@ -8,34 +8,101 @@ namespace Dapper.MoqTests
 
     public interface IMockDatabase
     {
+        /// <summary>
+        /// Executes a single-row query, returning the data typed as <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of result to return.</typeparam>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
+        /// <returns>
+        /// A sequence of data of the supplied type; if a basic type (int, string, etc) is queried then the data from the first column in assumed, otherwise an instance is
+        /// created per row, and a direct column-name===member-name mapping is assumed (case insensitive).
+        /// </returns>
         T QuerySingle<T>(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
+        /// <summary>
+        /// Executes a query, returning the data typed as <typeparamref name="T"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of results to return.</typeparam>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="buffered">Whether to buffer results in memory.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
+        /// <returns>
+        /// A sequence of data of the supplied type; if a basic type (int, string, etc) is queried then the data from the first column in assumed, otherwise an instance is
+        /// created per row, and a direct column-name===member-name mapping is assumed (case insensitive).
+        /// </returns>
         IEnumerable<T> Query<T>(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
+        /// <summary>
+        /// Execute parameterized SQL.
+        /// </summary>
+        /// <param name="sql">The SQL to execute for this query.</param>
+        /// <param name="param">The parameters to use for this query.</param>
+        /// <param name="transaction">The transaction to use for this query.</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout.</param>
+        /// <param name="commandType">Is it a stored proc or a batch?</param>
+        /// <returns>The number of rows affected.</returns>
         int Execute(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
+        /// <summary>
+        /// Execute a query asynchronously using .NET 4.5 Task.
+        /// </summary>
+        /// <typeparam name="T">The type of results to return.</typeparam>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
+        /// <returns>
+        /// A sequence of data of <typeparamref name="T"/>; if a basic type (int, string, etc) is queried then the data from the first column in assumed, otherwise an instance is
+        /// created per row, and a direct column-name===member-name mapping is assumed (case insensitive).
+        /// </returns>
         Task<IEnumerable<T>> QueryAsync<T>(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
+        /// <summary>
+        /// Execute a single-row query asynchronously using .NET 4.5 Task.
+        /// </summary>
+        /// <typeparam name="T">The type of result to return.</typeparam>
+        /// <param name="sql">The SQL to execute for the query.</param>
+        /// <param name="param">The parameters to pass, if any.</param>
+        /// <param name="transaction">The transaction to use, if any.</param>
+        /// <param name="commandTimeout">The command timeout (in seconds).</param>
+        /// <param name="commandType">The type of command to execute.</param>
         Task<T> QuerySingleAsync<T>(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
+        /// <summary>
+        /// Execute a command asynchronously using .NET 4.5 Task.
+        /// </summary>
+        /// <param name="sql">The SQL to execute for this query.</param>
+        /// <param name="param">The parameters to use for this query.</param>
+        /// <param name="transaction">The transaction to use for this query.</param>
+        /// <param name="commandTimeout">Number of seconds before command execution timeout.</param>
+        /// <param name="commandType">Is it a stored proc or a batch?</param>
+        /// <returns>The number of rows affected.</returns>
         Task<int> ExecuteAsync(
-            [ParameterType(ParameterType.SqlText)] string text,
-            [ParameterType(ParameterType.SqlParameters)] object parameters = null,
+            [ParameterType(ParameterType.SqlText)] string sql,
+            [ParameterType(ParameterType.SqlParameters)] object param = null,
             [ParameterType(ParameterType.SqlTransaction)] IDbTransaction transaction = null);
 
         DbTransaction BeginTransaction(IsolationLevel il);
